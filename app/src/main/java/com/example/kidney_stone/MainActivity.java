f55d65b1f4e418b29517e5843f7e1ca6f88564b6
Rohan <rohanpawar@gmail.com>
2024-01-14 17:43:00 +0530

    it is functionally taking place part for registration

--- app/src/main/java/com/example/kidney_stone/MainActivity.java
+++ app/src/main/java/com/example/kidney_stone/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.kidney_stone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    Button btn;
+    TextView text2;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth= FirebaseAuth.getInstance();
+        btn=findViewById(R.id.btn);
+        text2=findViewById(R.id.text2);
+        user=auth.getCurrentUser();
+        if(user==null){
+            Intent intent=new Intent(getApplicationContext(),login.class);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            text2.setText(user.getEmail());
+
+        }
+        text2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent=new Intent(getApplicationContext(),login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/kidney_stone/Signup.java
+++ app/src/main/java/com/example/kidney_stone/Signup.java
@@ -1,18 +1,43 @@
 package com.example.kidney_stone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Signup extends AppCompatActivity {
 TextInputEditText editTextEmail,editTextPassword;
+ProgressBar progressBar;
 Button buttonReg;
+FirebaseAuth mAuth;
+
+TextView textView;
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +46,24 @@ Button buttonReg;
         editTextEmail=findViewById(R.id.email);
         editTextPassword=findViewById(R.id.password);
         buttonReg=findViewById(R.id.btn_signup);
+        progressBar=findViewById(R.id.progress);
+        mAuth= FirebaseAuth.getInstance();
+        textView=findViewById(R.id.login_now);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),login.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
 
        buttonReg.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
+               progressBar.setVisibility(View.VISIBLE);
+
                String email,password;
                email=String.valueOf(editTextEmail.getText());
                password=String.valueOf(editTextPassword.getText());
@@ -37,7 +76,27 @@ Button buttonReg;
                    Toast.makeText(Signup.this, "enter password", Toast.LENGTH_SHORT).show();
                    return;
                }
+               mAuth.createUserWithEmailAndPassword(email, password)
+                       .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
 
+                           @Override
+                           public void onComplete(@NonNull Task<AuthResult> task) {
+                               progressBar.setVisibility(View.GONE);
+                               if (task.isSuccessful()) {
+
+                                   // Sign in success, update UI with the signed-in user's information
+                                   Toast.makeText(Signup.this, "Registration done", Toast.LENGTH_SHORT).show();
+                                   Intent intent=new Intent(getApplicationContext(),login.class);
+                                   startActivity(intent);
+                                   finish();
+                               } else {
+
+                                   // If sign in fails, display a message to the user.
+                                   Toast.makeText(Signup.this, "Failed To registration", Toast.LENGTH_SHORT).show();
+
+                               }
+                           }
+                       });
 
            }
        });
